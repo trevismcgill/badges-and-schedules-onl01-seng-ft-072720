b0d99e8b6baad3488bcf18cd604bef1c7c31e675
Trevis McGill <trevismcgill25@gmail.com>
2020-07-29 03:21:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,10 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{[name]}."
+end
+
 def batch_badge_creator(array)
   array.each do |name|
-    puts "Hello, my name is #{[name]}."
+
   end
 end

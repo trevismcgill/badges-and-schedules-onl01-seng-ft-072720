08f52c89c48cf61a6b805aa9f0ccec1304fc4649
Trevis McGill <trevismcgill25@gmail.com>
2020-07-29 03:31:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
+  badge_messages = []
   array.each do |badge_msg|
-    puts badge_maker(badge_msg)
+     badge_messages.push(badge_maker(badge_msg))
   end
+  return badge_messages
 end
